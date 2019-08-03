a0cf14aa4637957a119720c19b697597cc3c0cd5
Jennifer Alvarez <jenniffer.alvarez@ceiba.com.co>
2019-08-03 12:54:58 -0500

    se hacen pruebas frnt

--- tablero-front/src/app/app.component.spec.ts
+++ tablero-front/src/app/app.component.spec.ts
@@ -1,35 +1,68 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { Note } from './modelos/note.model';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NotasService } from './servicios/notas/notas.service';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { of } from 'rxjs';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  let notasServiceSpy = null;
+
+  const RespuestaNota = [
+    {title: 'Titulo',
+      body: 'Cuerpo',
+      author: 'autor',
+      created_at: 'fecha',
+      uuid: 'id344',
+    }
+  ] as Note[];
+
+  const RespuestaServicioOk = [];
   beforeEach(async(() => {
+    notasServiceSpy = jasmine.createSpyObj('NotasService', ['listar', 'crearNota', 'obtenerSegunId', 'eliminar']);
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule, HttpClientTestingModule, ReactiveFormsModule, FormsModule
+      ],providers: [
+        {provide: NotasService, useValue: notasServiceSpy}
       ],
+      schemas:[NO_ERRORS_SCHEMA],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'tablero-front'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('tablero-front');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  beforeEach(() => {
+    notasServiceSpy.listar.and.returnValue(of(RespuestaNota));
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to tablero-front!');
+  });
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('eliminar Nota', () => {
+    notasServiceSpy.eliminar.and.returnValue(of(RespuestaServicioOk));
+    component.deleteNote('id');
+    expect(notasServiceSpy.listar).toHaveBeenCalled();
+  });
+
+  it('deberia cerrar modal', () => {
+    component.dismiss();
+    expect(notasServiceSpy.listar).toHaveBeenCalled();
+  });
+
+  it('deberia abrir modal para ver detalles ', () => {
+    component.verDetalle('1');
+    expect(component.idNote).toBe('1');
   });
 });
