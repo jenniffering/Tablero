fc3aeb5cfef4f04fe9f8f34de27e74109c087387
Jennifer Alvarez <jenniffer.alvarez@ceiba.com.co>
2019-08-15 09:12:27 -0500

    VEGASOFT-2116: Se pruebas

--- tablero/src/test/java/com/tablero/tablero/TableroApplicationTests.java
+++ tablero/src/test/java/com/tablero/tablero/TableroApplicationTests.java
@@ -1,16 +1,67 @@
 package com.tablero.tablero;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.tablero.tablero.model.Nota;
+import com.tablero.tablero.repo.RepositorioNota;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TableroApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	private RepositorioNota repository;
+	
+	
+	   @Autowired
+	    public void setRepository(RepositorioNota repository) {
+	        this.repository = repository;
+	    }
+	   
+	    @Test
+	    public void findAllNotesIsEmpty() {
+	        List<Nota> notas = repository.findAll();
+	       // assertNotNull(notas);
+	        assertTrue(notas.isEmpty());
+	    }
+	    
+	    @Test
+	    public void deleteNotaWithId() {
+	    	 Nota nota = new Nota("id", "Java 8 in Action", "Programming", "", "");
+	        repository.delete(nota);
+	        assertNotNull(nota);
+	    }
+	    
+	    @Test
+	    public <S> void findNoteById() {
+	    	 Nota nota = new Nota("id", "Java 8 in Action", "Programming", "", "");
+		        String id = "id";
+				Optional<Nota> note = repository.findById(id);
+	        assertNotNull(note);
+	    }
+	    
+	    @Test
+	    public void createNote() {
+	    	 Nota nota = new Nota("id", "Java 8 in Action", "Programming", "", "");
+	       Nota savedNota = repository.save(nota);
+	       String id = "id";
+			Optional<Nota> note = repository.findById(id);
+	        assertEquals("Java 8 in Action", note.get().getNombreNota());
+	   
+	    }
+	    
+		
 
 }
